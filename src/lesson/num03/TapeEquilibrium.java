3a570f1d2cdf3dd6c9ab12faf1c6364d405a2251
tildev <tildevelove@gmail.com>
2018-06-18 18:10:06 +0900

    codility - lesson3 TapeEquilibrium

---
+++ src/lesson/num03/TapeEquilibrium.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package lesson.num03;
+
+/*
+ * codility - lesson3 TapeEquilibrium
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(N) (not counting the storage required for input arguments).
+ */
+/**
+ * @author tildev
+ * @data 2018. 6. 18.
+ */
+public class TapeEquilibrium {
+	public int solution(int[] A) {
+		int aLen = A.length;
+		long leftSum = 0;
+		long rightSum = 0;
+
+		for (int i = 0; i < aLen; i++) {
+			rightSum += A[i];
+		}
+		long minDiff = Integer.MAX_VALUE;
+		long diff = 0;
+		for (int i = 0; i < aLen-1; i++) {
+			leftSum += A[i];
+			rightSum -= A[i];
+			
+			diff = Math.abs(leftSum - rightSum);
+			if(diff<minDiff) {
+				minDiff = diff;
+			}
+		}
+		return (int) minDiff;
+	}
+
+	public static void main(String[] args) {
+		int[] A = { -1000, 1000 };
+		// int[] A = { 3, 1, 2, 4, 3 };
+		int i = new TapeEquilibrium().solution(A);
+		System.out.println(i);
+	}
+}
