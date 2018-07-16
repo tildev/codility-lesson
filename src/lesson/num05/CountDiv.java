554001070f84c1990f2eaba040eeeeb9f1872096
tildev <tildevelove@gmail.com>
2018-07-16 16:10:01 +0900

    codility - lesson5 CountDiv 예문 수정

--- src/lesson/num05/CountDiv.java
+++ src/lesson/num05/CountDiv.java
@@ -27,32 +27,13 @@ public class CountDiv {
 		return cnt;
 	}
 
-	/**
-	 * Time Out Error...
-	 * 
-	 * @param A
-	 * @param B
-	 * @param K
-	 * @return
-	 */
-	public int solutionTimeOutError(int A, int B, int K) {
-		int cnt = 0;
-
-		for (int i = A; i <= B; i++) {
-			if (i % K == 0) {
-				cnt++;
-			}
-		}
-		return cnt;
-	}
-
 	public static void main(String[] args) {
-		int A = 6;
-		int B = 11;
-		int K = 2;
-		// int A = 11;
-		// int B = 345;
-		// int K = 17;
+		int A = 0;
+		int B = 0;
+		int K = 11;
+//		int A = 6;
+//		int B = 11;
+//		int K = 2;
 		int result = new CountDiv().solution(A, B, K);
 		System.out.println(result);
 	}
